e196861b8452a57cb0525d7ee59af996d56c90e8
foliveira <fabio.an.oliveira@gmail.com>
2015-04-08 14:35:12 +0100

    Fix for #2
    * Fixes issue #2 - it was due to a mislabeled variable (and no proper testing)
    * Added a bunch of files
    * Added files directive to package.json

---
+++ examples/unfixed.js
@@ -0,0 +1,14 @@
+function bubbleSort(array, ascending) {
+ascending = (typeof ascending === 'undefined') || ascending;
+for(var i = 0; i < array.length; i++){
+for(var j = array.length - 1; j > i; j--){
+if(ascending ? array[j] < array[j - 1] : array[j - 1] < array[j]){
+var temp = array[j];
+array[j] = array[j - 1];
+array[j - 1] = temp;
+}
+}
+}
+
+return array;
+}
